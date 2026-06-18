9f0fa2ca178c16b00ee023f49d9e8d4c4f8dcf80
paulszyburski <paul.szyburski@gmail.com>
2026-06-18 20:26:44 +0200

    Basic Setup

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>The number 1 chess trainer</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 17
+    }
+})
